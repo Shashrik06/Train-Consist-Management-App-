8afab29a785b3ab925962189b1c710c5ad77cb51
Shashrik06 <shashrikmahendran@gmail.com>
2026-04-01 14:43:49 +0530

    Initial commit

---
+++ TrainApp.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+public class TrainApp {
+    public static void main(String[] args) {
+
+    }
+}
+ System.out.println("=== Train Consist Management App ===");
+List<String> trainConsist = new ArrayList<>();
+  System.out.println("Train consist initialized successfully.");
+     System.out.println("Initial bogie count: " + trainConsist.size());
+        }
+        }
